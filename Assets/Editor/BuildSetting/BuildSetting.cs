619361eb97aac4dc900f298e3c3e2672f7d889f3
kou_yeung <kou_yeung@hotmail.com>
2016-08-17 18:55:49 +0900

    ビルド設定メソッド追加
    MasterやDevelopのビルド前に実行されるメソッドを定義する。
    これらのメソッドを呼び出されることで、Build Settingを切り替えられるようになります。

---
+++ Assets/Editor/BuildSetting/BuildSetting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using System.Collections.Generic;
+
+public class BuildSetting
+{
+    [MenuItem("BuildSetting/Master")]
+    public static void Master()
+    {
+        SetScriptingDefineSymbols("");
+    }
+
+    [MenuItem("BuildSetting/Develop")]
+    public static void Develop()
+    {
+        SetScriptingDefineSymbols("UNITY_DEBUG");
+    }
+
+    static void SetScriptingDefineSymbols(string definds)
+    {
+        var group = EditorUserBuildSettings.selectedBuildTargetGroup;
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(group, definds);
+    }
+}
