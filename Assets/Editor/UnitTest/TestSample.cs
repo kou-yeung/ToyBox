c79ab083972e4a7f1505b51f39753e3a1e0e2a0d
kou_yeung <kou_yeung@hotmail.com>
2016-08-22 23:56:13 +0900

    Editor Testsサンプル追加

---
+++ Assets/Editor/UnitTest/TestSample.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEditor.EditorTests;
+using System;
+
+public class TestSample
+{
+    [TestCase]
+    public void Test()
+    {
+        Assert.AreEqual(3, 1 + 2);
+    }
+}
